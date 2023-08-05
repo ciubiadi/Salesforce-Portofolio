e457ace43fe68a34aef69f9f3565cb97ee11bc92
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-05 14:50:09 +0300

    2 ways data biinding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    fullname = 'Adrian Albert Ciubucciu'
+    users = ["a", "b"]
+    obj={
+        name: 'obj name',
+        age: 20
+    }
+    title="LWC Course"
+
+    //call the method in the class trough the lifecycle hook
+
+    //method
+    // getName(){
+    //     //perform my logic
+    //     console.log('this.fullname');
+    //     console.log(this.fullname);
+    // }
+
+    changeHandler(event){
+        this.title = event.target.value;
+    }
+}
