f4f9804350b952239081a4ab7ecf50999b30254a
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-05 21:17:10 +0300

    practice different looping

--- force-app/main/default/lwc/helloLooping/helloLooping.js
+++ force-app/main/default/lwc/helloLooping/helloLooping.js
@@ -2,4 +2,22 @@ import { LightningElement } from 'lwc';
 
 export default class HelloLooping extends LightningElement {
     productsList = ["Burger", "Pizza", "Sushi", "Noodles"];
+
+    userList = [
+        {
+            id: 1,
+            name: 'Adrian C',
+            email: 'adrian@ciub.com'
+        },
+        {
+            id: 2,
+            name: 'Cristian B',
+            email: 'cristian@gmail.com'
+        },
+        {
+            id: 3,
+            name: 'Robert D',
+            email: 'robert@gmail.com'
+        }
+    ]    
 }
