ce00df44b4514ec5d0431fa695f3f26edf29ef27
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-10 22:37:21 +0300

    Child to parent using event Bubbling

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -4,9 +4,14 @@ export default class C2pModalComponent extends LightningElement {
     clickHandler(){
         // detail property is a standard property so I can't use another keyword
         const customEvent = new CustomEvent('closemodal', {
+            bubbles: true,
             detail: {
                 msg: 'Modal Closed Succesfully!'}
         });
         this.dispatchEvent(customEvent);
+    }
+
+    footerHandler(){
+        console.log('footerHandler called');
     }
 }
