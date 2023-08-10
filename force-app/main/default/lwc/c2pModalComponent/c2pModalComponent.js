53702e13abaf802e36e7e6d7cddd10ee4eea5386
ciubuadi <ciubucciu.adrianalbert@gmail.com>
2023-08-10 22:29:30 +0300

    Child to parent using event with DATA

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -2,7 +2,11 @@ import { LightningElement } from 'lwc';
 
 export default class C2pModalComponent extends LightningElement {
     clickHandler(){
-        const customEvent = new CustomEvent('closemodal');
+        // detail property is a standard property so I can't use another keyword
+        const customEvent = new CustomEvent('closemodal', {
+            detail: {
+                msg: 'Modal Closed Succesfully!'}
+        });
         this.dispatchEvent(customEvent);
     }
 }
--- force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -2,8 +2,10 @@ import { LightningElement } from 'lwc';
 
 export default class C2pParentComponent extends LightningElement {
     showModal = false;
+    message = ''
 
-    toggleModal(){
+    toggleModal(event){
+        this.message = event.detail.msg;
         this.showModal = !this.showModal;
     }
 }
